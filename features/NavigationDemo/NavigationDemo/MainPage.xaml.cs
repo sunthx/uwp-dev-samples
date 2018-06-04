21d1a285f171bcd15d0346a098f1aceb95869716
sunthx <sunth0101@126.com>
2018-06-04 17:50:58 +0800

    navigation demo

---
+++ features/NavigationDemo/NavigationDemo/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace NavigationDemo
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            NaviagetionService.NavFrame = RootFrame;
+        }
+
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem as ListViewItem;
+            if (item == null)
+                return;
+
+            switch (item.Content)
+            {
+                case "Page1":
+                    NaviagetionService.NavToPage1();
+                    break;
+                case "Page2":
+                    NaviagetionService.NavToPage2();
+                    break;
+            }
+        }
+
+        private void FrameworkElement_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            (sender as ListView).SelectedIndex = 0;
+        }
+    }
+}
---
+++ features/NavigationDemo/NavigationDemo/NaviagetionService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using NavigationDemo.Pages;
+
+namespace NavigationDemo
+{
+    public static class NaviagetionService
+    {
+        static NaviagetionService()
+        {
+            SystemNavigationManager.GetForCurrentView().BackRequested += NaviagetionService_BackRequested;
+        }
+
+        private static void NaviagetionService_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            GoBack();
+            e.Handled = true;
+        }
+
+        public static Frame NavFrame { get; set; }
+
+        public static void NavToPage1()
+        {
+            NavFrame.Navigate(typeof(Page1), $"Page1 : Navigated At {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            ResetBackButtonState();
+        }
+
+        public static void NavToPage2()
+        {
+            NavFrame.Navigate(typeof(Page2), $"Page2 : Navigated At {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            ResetBackButtonState();
+        }
+
+        public static void GoBack()
+        {
+            if(NavFrame.CanGoBack)
+                NavFrame.GoBack();
+
+            ResetBackButtonState();
+        }
+
+        public static void ResetBackButtonState()
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = NavFrame.CanGoBack
+                ? AppViewBackButtonVisibility.Visible
+                : AppViewBackButtonVisibility.Collapsed;
+        }
+    }
+}
